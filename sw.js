4375e872e99fc92f1668733d41a2f050831bc46a
deepakag285-stack <deepakag285@gmail.com>
2026-06-09 08:19:05 +0530

    J

--- sw.js
+++ sw.js
@@ -14,3 +14,14 @@ self.addEventListener('fetch', (event) => {
     // अभी हम सीधे इंटरनेट से फाइलें लोड होने दे रहे हैं
     event.respondWith(fetch(event.request));
 });
+self.addEventListener('push', (event) => {
+  const options = {
+    body: event.data ? event.data.text() : 'Naya update aaya hai!',
+    icon: 'image/IMG-20260418-WA0001(1).jpg"',
+    badge: 'image/IMG-20260418-WA0001(1).jpg"'
+  };
+
+  event.waitUntil(
+    self.registration.showNotification('PWA Notification', options)
+  );
+});
